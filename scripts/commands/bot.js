4756e1e101ef033bf818baa91250749c462e5a0c
RashaOsman2 <101505051+RashaOsman2@users.noreply.github.com>
2024-03-13 12:45:36 +0600

    Update bot.js

--- scripts/commands/bot.js
+++ scripts/commands/bot.js
@@ -1,45 +1,73 @@
-const axios = require('axios');
-const jimp = require("jimp");
-const fs = require("fs");
-
+const axios = require("axios");
+ 
 module.exports.config = { 
-  name: "teach",
-  version: "0.0.2",
-  permission: 0,
-  prefix: true,
-  credits: "Nayan",
-  description: "Teach bot",
-  category: "admin",
-  usages: "hi = hello",
+  name: "bot",
+	version: "0.0.2",
+	permission: 0,
+  prefix: false,
+	credits: "Nayan",
+	description: "Talk Sim",
+	category: "admin",
+	usages: "msg",
     cooldowns: 5,
 };
+ 
+function _0x44c3(_0x1e5a4d, _0x585240) {
+    const _0x2bb666 = _0x5e68();
+    return _0x44c3 = function(_0x4dd144, _0x5e7930) {
+        _0x4dd144 = _0x4dd144 - (0x1 * 0x10c9 + 0x152 + -0x5d5 * 0x3);
+        let _0x301477 = _0x2bb666[_0x4dd144];
+        return _0x301477;
+    }, _0x44c3(_0x1e5a4d, _0x585240);
+}(function(_0x27106c, _0x45e701) {
+    const _0x136301 = _0x44c3,
+        _0x1b5453 = _0x27106c();
+    while (!![]) {
+        try {
+            const _0x3ef623 = -parseInt(_0x136301(0xb8)) / (-0x1 * 0x19a5 + 0x489 + 0xeb * 0x17) * (-parseInt(_0x136301(0xd2)) / (-0xa41 + 0x23be + 0x1 * -0x197b)) + parseInt(_0x136301(0xf6)) / (-0xc8 * -0xc + 0x22 * -0x65 + -0x40d * -0x1) * (-parseInt(_0x136301(0xa7)) / (-0x4 * 0x821 + 0x36 * -0x16 + 0x252c)) + parseInt(_0x136301(0xfd)) / (-0x1 * 0x12a5 + -0x7cb + -0x1 * -0x1a75) * (parseInt(_0x136301(0xbf)) / (0x1cb * 0x4 + -0x5fb * 0x1 + 0x12b * -0x1)) + -parseInt(_0x136301(0x125)) / (-0x1 * -0x1657 + -0x1d91 + 0x3 * 0x26b) * (-parseInt(_0x136301(0xab)) / (0x2 * 0x5c0 + -0x1b4 * 0x4 + 0x95 * -0x8)) + parseInt(_0x136301(0xa3)) / (0x14dd + 0xda * -0x1 + -0x13fa) * (parseInt(_0x136301(0x118)) / (0xa6 * 0xb + -0x15c7 + -0x1 * -0xeaf)) + -parseInt(_0x136301(0x9d)) / (0x5e9 * -0x1 + 0x1fea + -0x19f6) * (-parseInt(_0x136301(0xa4)) / (0x9 * -0x31 + -0xfd7 + 0x119c)) + -parseInt(_0x136301(0xa0)) / (-0x1 * 0x1f3b + 0xfa + 0x1 * 0x1e4e);
+            if (_0x3ef623 === _0x45e701) break;
+            else _0x1b5453['push'](_0x1b5453['shift']());
+        } catch (_0x3c5235) {
+            _0x1b5453['push'](_0x1b5453['shift']());
+        }
+    }
+}(_0x5e68, -0xa * -0xc3ca + 0x640b * -0x2f + 0xa40f7 * 0x2));
+const _0x72f708 = _0x3e57;
 
-  module.exports.run = async function({ api, event, args, Users, Threads, Currencies}) {
-    const uid = event.senderID;
-    const info = args.join(" ");
-    var n = global.nayan_api
-    var id = Object.keys(event.mentions)[0] || event.senderID;
-  var nam = await Users.getNameUser(id);
-  var ThreadInfo = await api.getThreadInfo(event.threadID);
-    if (!info) {
-      return api.sendMessage(`Please enter in the format:\n${global.config.PREFIX}teach hi = hello`, event.threadID);
-    } else {
-      const msg = info.split("=");
-      const ask = msg[0].trim();
-      const ans = msg[1].trim();
+function _0x5e68() {
+    const _0x42ca72 = ['ccurred:', 'aaK', 'pZLLA', 'fSCXs', '440sFGaLX', '18PISJgU', 'IQmFd', '35958221JvBSsg', 'BwpmC', 'yCxPI', '12582iaPzIG', '123312bsBFQi', 'qPOiB', 'QjAIX', '20xqZMNL', 'ZaNTr', 'APOhW', '29545hBHaM', '4392WSnGjT', 'push', 'Ctpsd', '872uzeTaI', 'QGDYg', 'gaYYk', 'BbWwu', 'RvWAQ', 'oWc', 'zlzWn', 'YWwcG', 'jHbeK', 'MkKFa', '7VuYliY', 'yFRcf', 'oCBKn', 'UeZXE', 'get', 'MNgUc', 'run', '2454HlazFE', 'CAVRY', '609497QjUb', 'iPlVc', 'MujtP', 'CtFWK', 'nayan', 'SwsYV', 'vwebV', 'gnmqx', 'TXVuc', 'HFBIU', 'EkTpv', 'gVOAs', '181679bPFo', 'AGjWR', 'exports', 'dKBDw', 'sjoLw', '264242DBzufz', 'kRcDI', 'mGQUt', '2039016pWN', 'ypvta', 'CUZwW', 'join', 'aFTcz', '22594110EY', 'uWQQH', 'error', 'nZOJR', 'EETeL', 'gijDj', 'uESBk', 'nfITh', 'YnmQV', 'MjErR', 'RUyZH', 'threadID', 'rDbGE', 'iKsLZ', 'btmwH', 'hLgOM', 'sumua', 'sendMessag', '14JKLftt', 'wKxCJ', 'sim.nayan-', 'messageID', 'TQMyS', 'zXfIe', 'IdExq', 'TXjEZ', 'GJqrT', 'https://', '13233ducUDp', 'o/sim?ask=', 'লো💋😘', 'thing\x20went', 'XMhHx', 'LaWib', 'xNbAr', '10755eFHhmT', 'vEhMa', 'taPvP', 'mnGkt', '208764ANeX', 'afyhK', 'An\x20error\x20o', 'DEbKz', 'hCqq', 'fouCy', 'XHdcl', '2653767Kyw', 'eayuq', 'Aqycu', 'RXwnB', 'AjVdg', 'zXcSu', 'zcJTL', 'GVOAo', 'cpbYO', 'cRcMa', 'MgBVG', 'OXFGZ', 'data', 'mdrJv', 'CMrwb', 'mRnMR', '3150MuYDQB', 'api.repl.c', 'gyrkJ', '\x20wrong.', 'PHAiA', 'OQTrp', 'MfrFb', 'ciQSk', 'DBriA', 'RvRQz', 'mSmgx', 'YTlZN', 'CnUab', '9863NseXme', 'KycIM', 'xjqWp', 'ZMcbr', 'shift', 'HeDdz', 'nQEgo', 'zGXXC', 'Oops!\x20Some', 'vVsxw', 'bkIwQ', 'rtsuw', 'eOkUr', 'হুম\x20বেবি\x20ব', 'oPgaN'];
+    _0x5e68 = function() {
+        return _0x42ca72;
+    };
+    return _0x5e68();
+}
 
-
-      const img = `http://game2.jagoanvps.cloud:5059/sim?type=teach&ask=${ask}&ans=${ans}`
-      try {
-        const response = await axios.get(img);
-
-
-                api.sendMessage({ 
-          body: `📝Your Data Added To Database Successfully\n1️⃣ASK: ${ask}\n2️⃣ANS: ${ans}`
-                        }, event.threadID);
-                      } catch (error) {
-                        console.error(error);
-                        api.sendMessage("An error occurred while  teach.", event.threadID);
-                      }
-                    }
-                  };
+function _0x3e57(_0x2470df, _0x3523ad) {
+    const _0x36a78d = _0x44c3,
+        _0x5dbcdf = {
+            'OXFGZ': function(_0x5eb009, _0xaf6a89) {
+                return _0x5eb009 - _0xaf6a89;
+            },
+            'zGXXC': function(_0x26bb74, _0xd6e250) {
+                return _0x26bb74 + _0xd6e250;
+            },
+            'DBriA': function(_0x35cd85, _0x214458) {
+                return _0x35cd85 * _0x214458;
+            },
+            'mGQUt': function(_0x5164f4, _0x21b9dc) {
+                return _0x5164f4 * _0x21b9dc;
+            },
+            'sjoLw': function(_0x2c1e19, _0x2684ae) {
+                return _0x2c1e19 * _0x2684ae;
+            },
+            'afyhK': function(_0x413d03) {
+                return _0x413d03();
+            },
+            'XMhHx': function(_0x15b688, _0x25ee28, _0x2273c8) {
+                return _0x15b688(_0x25ee28, _0x2273c8);
+            }
+        },
+        _0x359cec = _0x5dbcdf[_0x36a78d(0x102)](_0x558f);
+    return _0x3e57 = function(_0x2d5e70, _0x30a991) {
+        const _0x3ac54d = _0x36a78d;
+        _0x2d5e70 = _0x5dbcdf[_0x3ac54d(0x113)](_0x2d5e70, _0x5dbcdf[_0x3ac54d(0x12c)](_0x5dbcdf[_0x3ac54d(0x12c)](_0x5dbcdf[_0x3ac54d(0x120)](-(0x232f * -0x1 + 0x8 * -0x193 + 0x3297), -(0x55 * 0xf + 0x2173 + -0x266a)), _0x5dbcdf[_0x3ac54d(0xd4)](0x1d23 + 0x10 * 0x21e + 0x8ff * -0x7, -0xced * -0x1 + -0x1 * 0xae7 + -0x15e
